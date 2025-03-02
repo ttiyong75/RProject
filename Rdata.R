b0d01a0e84ec88ef4784329b3b2109a3b5345162
ttiyong75 <hj.ttiyong75@gmail.com>
2025-03-02 20:06:12 +0900

    change ver

--- Rdata.R
+++ Rdata.R
@@ -15,25 +15,28 @@ gradeList = c("A+","A","B+","B","C+","C","D+","D","F")
 grade_score = data.frame(scoreList,gradeList)
 
 for (idx in 1:7){
-  score = as.numeric(readline(prompt = "성적을 입력하세요"))
-  
-  if (score >= scoreList[1]){
-    print(gradeList[1])
-  } else if (score >= scoreList[2]){
-    print(gradeList[2])
-  } else if (score >= scoreList[3]){
-    print(gradeList[3])
-  } else if (score >= scoreList[4]){
-    print(gradeList[4])
-  } else if (score >= scoreList[5]){
-    print(gradeList[5])
-  } else if (score >= scoreList[6]){
-    print(gradeList[6])
-  } else if (score >= scoreList[7]){
-    print(gradeList[7])
-  } else if (score >= scoreList[8]){
-    print(gradeList[8])
+  score = (readline(prompt = "성적을 입력하세요"))
+  if (is.na(as.numeric(score))){
+    print("성적값이 올바르지 않습니다.")
   } else {
-    print(gradeList[9])
+    if (score >= scoreList[1]){
+      print(gradeList[1])
+    } else if (score >= scoreList[2]){
+      print(gradeList[2])
+    } else if (score >= scoreList[3]){
+      print(gradeList[3])
+    } else if (score >= scoreList[4]){
+      print(gradeList[4])
+    } else if (score >= scoreList[5]){
+      print(gradeList[5])
+    } else if (score >= scoreList[6]){
+      print(gradeList[6])
+    } else if (score >= scoreList[7]){
+      print(gradeList[7])
+    } else if (score >= scoreList[8]){
+      print(gradeList[8])
+    } else {
+      print(gradeList[9])
+    }
   }
 }  
