2e0cd0826ecfa8b155ad989cf2d49df823997e0d
ttiyong75 <hj.ttiyong75@gmail.com>
2025-03-22 14:33:29 +0900

    example

---
+++ exam1.R
@@ -0,0 +1,27 @@
+te
+
+if (v1[1]!=2){
+  print(v1[1])
+}
+
+for (idx in v2) {
+  print(idx * 10)
+}
+
+for (idx in v2) {
+  calc = 2*idx+3
+  print(calc)
+}
+
+fncTest=function(wei,hei){
+  varBmi = wei/(hei/100)^2
+  print(varBmi)
+} 
+
+fncTest(66,165)
+
+sex = c("남","남","남","남","남","남","여","여","여","여")
+hie = c(170,175,160,180,200,188,170,165,155,150)
+
+
+
